fd2f710e00c5141016018ec95ea7e37b877689a3
Johan Swan <jswannieman@gmail.com>
2024-03-27 17:15:17 +1300

    Commit App JSX Component

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import { ThemeContextProvider } from "./contexts/themeContext";
+import Card from "./components/Card";
+
+function App() {
+  const [themeDark, setThemeDark] = useState(false);
+
+  useEffect(() => {
+    if (!themeDark) {
+      document.body.style.backgroundColor = "rgb(48, 44, 44)";
+      document.querySelector(".card").style.backgroundColor =
+        "rgb(149, 143, 143)";
+    } else {
+      document.body.style.backgroundColor = "rgb(149, 143, 143)";
+      document.querySelector(".card").style.backgroundColor = "rgb(48, 44, 44)";
+    }
+  }, [themeDark]);
+
+  return (
+    <ThemeContextProvider value={{ themeDark }}>
+      <div>
+        <button onClick={() => setThemeDark(!themeDark)}>
+          Change the Theme
+        </button>
+        <Card />
+      </div>
+    </ThemeContextProvider>
+  );
+}
+
+export default App;
